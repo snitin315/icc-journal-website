a6097cbb79cf5c96d98acdde1f4ba0ad3ae2c003
Nitin Kumar <snitin315@gmail.com>
2019-10-17 18:13:15 +0530

    updated navbar and headerlinks

--- components/Header/HeaderLinks.js
+++ components/Header/HeaderLinks.js
@@ -6,12 +6,10 @@ import Link from "next/link";
 import { makeStyles } from "@material-ui/core/styles";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
-import Tooltip from "@material-ui/core/Tooltip";
 
 // @material-ui/icons
-import { Apps, CloudDownload } from "@material-ui/icons";
-import DeleteIcon from "@material-ui/icons/Delete";
-import IconButton from "@material-ui/core/IconButton";
+import { Apps, School,Home,Book,GroupAdd,Money} from "@material-ui/icons";
+
 
 // core components
 import CustomDropdown from "components/CustomDropdown/CustomDropdown.js";
@@ -26,95 +24,87 @@ export default function HeaderLinks(props) {
   return (
     <List className={classes.list}>
       <ListItem className={classes.listItem}>
+        <Button
+          href="/"
+          color="transparent"
+          target="_blank"
+          className={classes.navLink}
+        >
+          <Home className={classes.icons} /> Home
+        </Button>
+      </ListItem>
+      <ListItem className={classes.listItem}>
+        <Button
+          href="/call-for-papers"
+          color="transparent"
+          target="_blank"
+          className={classes.navLink}
+        >
+          <Book className={classes.icons} /> Call For Papers
+        </Button>
+      </ListItem>
+
+      <ListItem className={classes.listItem}>
         <CustomDropdown
           noLiPadding
           navDropdown
-          buttonText="Components"
+          buttonText="Paper Submission"
           buttonProps={{
             className: classes.navLink,
             color: "transparent"
           }}
           buttonIcon={Apps}
           dropdownList={[
-            <Link href="/components">
-              <a className={classes.dropdownLink}>All components</a>
+            <Link href="/paper-submission/instruction-to-authors">
+              <a className={classes.dropdownLink}> Instruction To Authors</a>
             </Link>,
-            <a
-              href="https://creativetimofficial.github.io/nextjs-material-kit/#/documentation?ref=njsmk-navbar"
-              target="_blank"
-              className={classes.dropdownLink}
-            >
-              Documentation
-            </a>
+            <Link href="/paper-submission/peer-review-policy">
+            <a className={classes.dropdownLink}> Peer Review Policy</a>
+            </Link>,
+            <Link href="/paper-submission/plagiarism-policy">
+            <a className={classes.dropdownLink}> Plagiarism Policy</a>
+            </Link>,
+            <Link href="/paper-submission/paper-submission-template">
+            <a className={classes.dropdownLink}> Paper Submission template</a>
+           </Link>,
+            <Link href="/paper-submission/copyright-form">
+            <a className={classes.dropdownLink}> Copyright Form</a>
+           </Link>
+
           ]}
         />
       </ListItem>
       <ListItem className={classes.listItem}>
         <Button
-          href="https://www.creative-tim.com/product/nextjs-material-kit?ref=njsmk-navbar"
+          href="/editorial-board"
           color="transparent"
-          target="_blank"
           className={classes.navLink}
         >
-          <CloudDownload className={classes.icons} /> Download
+          <Book className={classes.icons} /> Editorial Board
         </Button>
       </ListItem>
+
       <ListItem className={classes.listItem}>
-        {/*<Tooltip title="Delete">
-          <IconButton aria-label="Delete">
-            <DeleteIcon />
-          </IconButton>
-        </Tooltip>*/}
-        <Tooltip
-          id="instagram-twitter"
-          title="Follow us on twitter"
-          placement={"top"}
-          classes={{ tooltip: classes.tooltip }}
+        <Button
+          href="/archieve"
+          color="transparent"
+          className={classes.navLink}
         >
-          <Button
-            href="https://twitter.com/CreativeTim?ref=creativetim"
-            target="_blank"
-            color="transparent"
-            className={classes.navLink}
-          >
-            <i className={classes.socialIcons + " fab fa-twitter"} />
-          </Button>
-        </Tooltip>
+          <Book className={classes.icons} /> Archieve
+        </Button>
       </ListItem>
+
       <ListItem className={classes.listItem}>
-        <Tooltip
-          id="instagram-facebook"
-          title="Follow us on facebook"
-          placement={"top"}
-          classes={{ tooltip: classes.tooltip }}
+        <Button
+          href="/indexing"
+          color="transparent"
+          className={classes.navLink}
         >
-          <Button
-            color="transparent"
-            href="https://www.facebook.com/CreativeTim?ref=creativetim"
-            target="_blank"
-            className={classes.navLink}
-          >
-            <i className={classes.socialIcons + " fab fa-facebook"} />
-          </Button>
-        </Tooltip>
+          <Book className={classes.icons} /> Indexing
+        </Button>
       </ListItem>
-      <ListItem className={classes.listItem}>
-        <Tooltip
-          id="instagram-tooltip"
-          title="Follow us on instagram"
-          placement={"top"}
-          classes={{ tooltip: classes.tooltip }}
-        >
-          <Button
-            color="transparent"
-            href="https://www.instagram.com/CreativeTimOfficial?ref=creativetim"
-            target="_blank"
-            className={classes.navLink}
-          >
-            <i className={classes.socialIcons + " fab fa-instagram"} />
-          </Button>
-        </Tooltip>
-      </ListItem>
+      
+      
     </List>
   );
 }
--- pages/_app.js
+++ pages/_app.js
@@ -78,7 +78,7 @@ export default class MyApp extends App {
     return (
       <React.Fragment>
         <Head>
-          <title>NextJS Material Kit by Creative Tim</title>
+          <title>ICC-Journal</title>
         </Head>
         <Component {...pageProps} />
       </React.Fragment>
