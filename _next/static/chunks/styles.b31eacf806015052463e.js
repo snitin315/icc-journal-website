1f90a4f0ad293ccc46809ab72d5a1ff5b3f63f55
Nitin Kumar <snitin315@gmail.com>
2020-03-27 14:16:30 +0530

    Updates

---
+++ _next/static/chunks/styles.b31eacf806015052463e.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[16],{ADjO:function(n,w,o){}}]);
---
+++ _next/static/webpack/static/development/pages/_error.js.895b2b40b0d1e81af6e2.hot-update.js
@@ -0,0 +1,109 @@
+webpackHotUpdate("static/development/pages/_error.js",{
+
+/***/ "./node_modules/@babel/runtime/helpers/esm/assertThisInitialized.js":
+false,
+
+/***/ "./node_modules/@babel/runtime/helpers/esm/classCallCheck.js":
+false,
+
+/***/ "./node_modules/@babel/runtime/helpers/esm/createClass.js":
+false,
+
+/***/ "./node_modules/@babel/runtime/helpers/esm/defineProperty.js":
+false,
+
+/***/ "./node_modules/@babel/runtime/helpers/esm/getPrototypeOf.js":
+false,
+
+/***/ "./node_modules/@babel/runtime/helpers/esm/inherits.js":
+false,
+
+/***/ "./node_modules/@babel/runtime/helpers/esm/possibleConstructorReturn.js":
+false,
+
+/***/ "./node_modules/@babel/runtime/helpers/esm/setPrototypeOf.js":
+false,
+
+/***/ "./node_modules/@babel/runtime/helpers/esm/typeof.js":
+false,
+
+/***/ "./node_modules/native-url/dist/index.js":
+false,
+
+/***/ "./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2F_error&absolutePagePath=%2Froot%2FProjects%2Ficc-journal-website%2Fpages%2F_error.js!./":
+false,
+
+/***/ "./node_modules/next/dist/client/router.js":
+false,
+
+/***/ "./node_modules/next/dist/client/with-router.js":
+false,
+
+/***/ "./node_modules/next/dist/next-server/lib/mitt.js":
+false,
+
+/***/ "./node_modules/next/dist/next-server/lib/router-context.js":
+false,
+
+/***/ "./node_modules/next/dist/next-server/lib/router/router.js":
+false,
+
+/***/ "./node_modules/next/dist/next-server/lib/router/utils/is-dynamic.js":
+false,
+
+/***/ "./node_modules/next/dist/next-server/lib/router/utils/route-matcher.js":
+false,
+
+/***/ "./node_modules/next/dist/next-server/lib/router/utils/route-regex.js":
+false,
+
+/***/ "./node_modules/next/dist/next-server/lib/utils.js":
+false,
+
+/***/ "./node_modules/next/node_modules/@babel/runtime/helpers/arrayWithHoles.js":
+false,
+
+/***/ "./node_modules/next/node_modules/@babel/runtime/helpers/construct.js":
+false,
+
+/***/ "./node_modules/next/node_modules/@babel/runtime/helpers/interopRequireWildcard.js":
+false,
+
+/***/ "./node_modules/next/node_modules/@babel/runtime/helpers/iterableToArrayLimit.js":
+false,
+
+/***/ "./node_modules/next/node_modules/@babel/runtime/helpers/nonIterableRest.js":
+false,
+
+/***/ "./node_modules/next/node_modules/@babel/runtime/helpers/slicedToArray.js":
+false,
+
+/***/ "./node_modules/next/node_modules/@babel/runtime/regenerator/index.js":
+false,
+
+/***/ "./node_modules/next/node_modules/react-is/cjs/react-is.development.js":
+false,
+
+/***/ "./node_modules/next/node_modules/react-is/index.js":
+false,
+
+/***/ "./node_modules/querystring-es3/decode.js":
+false,
+
+/***/ "./node_modules/querystring-es3/encode.js":
+false,
+
+/***/ "./node_modules/querystring-es3/index.js":
+false,
+
+/***/ "./node_modules/regenerator-runtime/runtime.js":
+false,
+
+/***/ "./pages/_error.js":
+false,
+
+/***/ 3:
+false
+
+})
+//# sourceMappingURL=_error.js.895b2b40b0d1e81af6e2.hot-update.js.map
