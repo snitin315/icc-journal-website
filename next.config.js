eff03635dbebeff96e5d2dafa220ebd00f08376d
Nitin Kumar <snitin315@gmail.com>
2020-03-27 14:56:43 +0530

    deployment

--- next.config.js
+++ next.config.js
@@ -1,12 +1,8 @@
-const withPlugins = require("next-compose-plugins");
-const withImages = require("next-images");
-const withSass = require("@zeit/next-sass");
-const webpack = require("webpack");
 const path = require("path");
 
-module.exports = withPlugins([[withSass], [withImages]], {
-  webpack(config, options) {
+module.exports ={
+  webpack: (config, options) => {
     config.resolve.modules.push(path.resolve("./"));
     return config;
   }
-});
+};
