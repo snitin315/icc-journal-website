77d5ea835d2e6375de51b409f2858f2dea724bca
Nitin Kumar <snitin315@gmail.com>
2020-08-19 17:29:05 +0530

    add co-editor-in-cheif

--- pages-sections/EditorialBoard-Sections/editor-in-cheif.js
+++ pages-sections/EditorialBoard-Sections/editor-in-cheif.js
@@ -7,6 +7,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import GridContainer from 'components/Grid/GridContainer.js';
 import GridItem from 'components/Grid/GridItem.js';
 import editor from '../../assets/img/editorcheif.png';
+import coEditor from '../../assets/img/coeditor.jpeg';
 import styles from 'assets/jss/nextjs-material-kit/pages/landingPageSections/teamStyle.js';
 
 const useStyles = makeStyles(styles);
@@ -45,6 +46,28 @@ export default function EditorInCheif() {
           <h4 className={classes.description}>
             {' '}
             <a> Email: joeljr@inatel.br, joeljr@ieee.org. </a>{' '}
+          </h4>
+        </GridItem>
+      </GridContainer>
+
+      <h2 className={classes.title}> Co-Editor-in-Chief</h2>
+      <GridContainer justify="center">
+        <GridItem xs={12} sm={12} md={4}>
+          <img src={coEditor} style={{ maxWidth: '300px', height: '300px' }} />
+        </GridItem>
+        <GridItem xs={12} sm={12} md={8} style={{ textAlign: 'left' }}>
+          <h2 className={classes.title}> Vineet Kansal </h2>
+          <h3 className={classes.description}>Professor, IET, Lucknow</h3>
+          <h4 className={classes.description}>
+            {' '}
+            <b>
+              {' '}
+              Pro Vice Chancellor, Dr APJ Abdul Kalam Technical University, Lucknow, Uttar Pradesh
+            </b>{' '}
+          </h4>
+          <h4 className={classes.description}>
+            {' '}
+            <a> Email: vineetkansal@yahoo.com </a>{' '}
           </h4>
         </GridItem>
       </GridContainer>
